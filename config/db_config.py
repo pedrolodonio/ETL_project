d476e94202eed335d718ba59b1a1d51ff18b0e2a
Pedro <pedrolodonio20@gmail.com>
2025-02-17 00:47:03 -0300

    adicionando arquivos iniciais do projeto

---
+++ config/db_config.py
@@ -0,0 +1,9 @@
+DB_CONFIG = {
+    "host":"localhost",
+    "port":5432,
+    "database": "teste",
+    "user": "postgres",
+    "password": "****"
+}
+
+#Altere os dados para o banco de dados criado no Postgres
